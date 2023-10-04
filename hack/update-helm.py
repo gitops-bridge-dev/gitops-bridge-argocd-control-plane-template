6bd24d64e4bae75a6d6aa0ee51cad221441022e5
Carlos Santana <csantana23@gmail.com>
2023-10-03 23:07:42 -0400

    update hack script
    Signed-off-by: Carlos Santana <csantana23@gmail.com>

--- hack/update-helm.py
+++ hack/update-helm.py
@@ -4,22 +4,36 @@ import yaml
 import sys
 import subprocess
 from packaging import version
+from packaging.version import InvalidVersion
 import requests
 
 
 def compare_versions(current_version, latest_version, chart_name):
     # Compare the versions
-    if latest_version and version.parse(latest_version) > version.parse(current_version):
+    # print(f"Comparing versions for {chart_name}")
+    # print(f"Current version: {current_version}")
+    # print(f"Latest version: {latest_version}")
+    # prefix latest_version with v if it's not already
+    # convert latest_version to string
+    cmp_latest_version = f'{str(latest_version)}'
+    if not cmp_latest_version.startswith('v'):
+        cmp_latest_version = 'v' + str(latest_version)
+    if latest_version and version.parse(cmp_latest_version) > version.parse(current_version):
         print(
             f"Newer version available for {chart_name}: {latest_version} (current: {current_version})")
-        return latest_version
+        # if current_version starts with 'v' make sure that return value also start with a single 'v'
+        if current_version.startswith('v') and not f'{str(latest_version)}'.startswith('v'):
+            return 'v' + str(latest_version)
+        return str(latest_version)
     else:
       print(f"No newer version available for {chart_name} (current: {current_version})")
     return None
 
 
 def extract_values(yaml_file):
+    addon_chart_repository_namespace = None
     with open(yaml_file, 'r') as file:
+        print(f"Extracting values from {yaml_file}")
         content = yaml.safe_load(file)
         try:
             addon_chart = content['spec']['generators'][0]['merge']['generators'][0]['clusters']['values']['addonChart']
@@ -27,10 +41,25 @@ def extract_values(yaml_file):
                 'generators'][0]['clusters']['values']['addonChartVersion']
             addon_chart_repository = content['spec']['generators'][0]['merge'][
                 'generators'][0]['clusters']['values']['addonChartRepository']
-            return addon_chart, addon_chart_version, addon_chart_repository
+            # check for addonChartRepositoryNamespace without creating an exception if key is not present
+            # check if content contains addonChartRepositoryNamespace
+            if 'addonChartRepositoryNamespace' in content['spec']['generators'][0]['merge']['generators'][0]['clusters']['values']:
+                addon_chart_repository_namespace = content['spec']['generators'][0]['merge'][
+                'generators'][0]['clusters']['values']['addonChartRepositoryNamespace']
+                # check if addon_chart_repository_namespace is a None value, if it is, then set it to empty string
+
+            if addon_chart_repository_namespace is None:
+                addon_chart_repository_namespace = ""
+            # print the four return values before returning
+            # print(f"addon_chart: {addon_chart}")
+            # print(f"addon_chart_version: {addon_chart_version}")
+            # print(f"addon_chart_repository: {addon_chart_repository}")
+            # print(f"addon_chart_repository_namespace: {addon_chart_repository_namespace}")
+
+            return addon_chart, addon_chart_version, addon_chart_repository, addon_chart_repository_namespace
         except (KeyError, TypeError):
-            # print(f"Unable to extract values from {yaml_file}")
-            return None, None, None
+            print(f"Unable to extract values from {yaml_file}")
+            return None, None, None, None
 
 
 def check_newer_version(repo_url, chart_name, current_version):
@@ -124,16 +153,47 @@ def extract_provider_values(yaml_file):
 
 def check_newer_version_from_oci(provider_name, provider_registry, provider_version):
     # Execute the command and get the output
+    print(f'provider_registry: {provider_registry}')
+    print(f'provider_name: {provider_name}')
+    print(f'provider_version: {provider_version}')
     result = subprocess.getoutput(f'crane ls {provider_registry}')
 
     # Split the output by lines and parse the versions
-    versions = [version.parse(v) for v in result.strip().split('\n') if '-rc.' not in v]
+    # remove any line that cause an exception InvalidVersion
+    # iterate over evey line try version.parse with a try except block
+    # if the version.parse doesn't throw an exception, then add the line to the result
+    # if the version.parse throws an exception, then do nothing
+    new_result = ''
+    for line in result.strip().split('\n'):
+        try:
+            # if provider_name is karpenter ignore if the line does not start with 'v'
+            if provider_name == 'karpenter' and not line.startswith('v'):
+                continue
+            # line contains '-stable; ignore
+            if '-stable' in line:
+                continue
+            # line contains '-rc' ignore
+            if '-rc.' in line:
+                continue
+            version.parse(line)
+            new_result += f'{line}\n'
+        except (InvalidVersion):
+            continue
+
+    # if new_result is empty, then return None
+    if new_result == '':
+        return provider_version
+    # print new_result
+    # print(f'new_result: {new_result}')
+
+    # Split the output by lines and parse the versions
+    versions = [version.parse(v) for v in new_result.strip().split('\n')]
 
     # Find the latest version
     latest_version = max(versions)
 
     update_version = compare_versions(
-        provider_version, f'v{str(latest_version)}', provider_name)
+        provider_version, latest_version, provider_name)
 
     return update_version
 
@@ -164,16 +224,12 @@ for yaml_file in yaml_files:
         except (KeyError, TypeError, yaml.YAMLError):
             # print(f"Unable to extract values from {yaml_file}")
             continue
-    addon_chart, addon_chart_version, addon_chart_repository = extract_values(
+    addon_chart, addon_chart_version, addon_chart_repository, addon_chart_repository_namespace = extract_values(
         yaml_file)
     if addon_chart is not None:
-        if addon_chart == "karpenter":
-            update_version(yaml_file, addon_chart_version, check_newer_version_from_github(
-                "aws", "karpenter", addon_chart_version))
-            continue
-        if addon_chart == "aws-gateway-controller-chart":
-            update_version(yaml_file, addon_chart_version, check_newer_version_from_github(
-                "aws", "aws-application-networking-k8s", addon_chart_version))
+        # make the following a switch statement
+        if addon_chart_repository == "public.ecr.aws":
+            update_version(yaml_file, addon_chart_version,check_newer_version_from_oci(addon_chart, addon_chart_repository+"/"+addon_chart_repository_namespace+"/"+addon_chart, addon_chart_version))
             continue
         update_version(yaml_file, addon_chart_version, check_newer_version(
             addon_chart_repository, addon_chart, addon_chart_version))
